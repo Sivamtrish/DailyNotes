36b56568d984c18aa54f6a1d35417c04c9bc85ca
mavis-emiewo-android-fcmb <mavis.emiewo@fcmb.com>
2026-02-11 13:33:34 +0100

    Day 2 - Display single note on screen

--- app/src/main/java/com/example/dailynotes/MainActivity.kt
+++ app/src/main/java/com/example/dailynotes/MainActivity.kt
@@ -1,15 +1,33 @@
 package com.example.dailynotes
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+
+
+        val title = findViewById<TextView>(R.id.titleText)
+        val content= findViewById<TextView>(R.id.contentText)
+        val date= findViewById<TextView>(R.id.dateText)
+
+
+        val note = Note(
+            title = "First Note",
+            content = "Learning Kotlin and Android",
+            date = "2024-06-01"
+        )
+
+        title.text = note.title
+        content.text= note.content
+        date.text= note.date
     }
+
 }
